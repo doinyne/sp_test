93097f44a5383238fc2ed43acccca5cb8d3b04ae
dean <d.lewis.rave@gmail.com>
2022-04-01 21:24:49 +0100

    the server.log is living within a method at the moment. Next move, making it into an argument

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,8 +1,19 @@
-server = File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log")
-file_data = server.read
-puts file_data
-
 class Parsing
-  def parse
+
+  def file
+    parser = File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
   end
+
+  def parse
+    parser = File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
+    file_data = parser.read
+    puts file_data
+  end
+
+  def info
+    parser = File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
+    file_data = parser.size
+    puts file_data
+  end
+
 end
