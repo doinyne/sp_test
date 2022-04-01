a1bba1919827d2ef33f5f3018f470ff06d771b10
dean <d.lewis.rave@gmail.com>
2022-04-01 18:02:00 +0100

    can view the webserver.log file

---
+++ lib/parser.rb
@@ -0,0 +1,13 @@
+class Parsing
+  def add
+    return 2 + 3
+  end
+  web_server_logger = 
+
+
+
+  def view_server_log
+    web_server_logger
+  end
+
+end
--- lib/parsing.rb
+++
@@ -1,5 +0,0 @@
-class Parsing
-  def parse
-    return 2 + 3
-  end
-end
