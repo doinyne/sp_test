0f7aacc3d7ae3b7014bc86dde5c3fe12bff9820a
dean <d.lewis.rave@gmail.com>
2022-04-01 21:42:07 +0100

    unsaved file

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,13 +5,11 @@ class Parsing
   end
 
   def parse
-    parser = File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
     file_data = parser.read
     puts file_data
   end
 
   def info
-    parser = File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
     file_data = parser.size
     puts file_data
   end
