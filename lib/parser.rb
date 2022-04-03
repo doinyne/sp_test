7573baacfc1a075a8df979d9c4d4a57a498e5176
dean <d.lewis.rave@gmail.com>
2022-04-03 20:25:45 +0100

    found count of whole log

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,7 @@
 class Parsing
+  
   def file
-    parser = File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
+    File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
   end
 
   def reader(func = method(:file))
--- lib/searching.rb
+++ lib/searching.rb
@@ -1,6 +1,20 @@
-require 'parser.rb'
-
 class Searching
-  def method_name
+
+  def file
+    File.open("/Users/deanlewis/Desktop/Smart_Pension_-_Take_Home_Ruby_test__7_/ruby_app/webserver.log", "r")
   end
+
+  def reader(func = method(:file))
+    file.read
+  end
+  
+  # i need a hash to return how many times a word appears in the string / log file
+  def word_list
+    list = Hash.new(0)
+  end
+  
+  def amount_of_words
+    reader.scan(/\w+/)
+  end
+  
 end
