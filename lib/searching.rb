71be67ffe779f3a039d26f1ca88ae0f7834d8141
dean <d.lewis.rave@gmail.com>
2022-04-03 20:47:00 +0100

    counts number of visits for each page

--- lib/searching.rb
+++ lib/searching.rb
@@ -7,14 +7,19 @@ class Searching
   def reader(func = method(:file))
     file.read
   end
+
+  #remove integers from log file
+  def remove_num 
+    reader.gsub!(/\d+/,"")
+  end
   
   # i need a hash to return how many times a word appears in the string / log file
   def word_list
     list = Hash.new(0)
-    reader.scan(/\w+/) { |word| list[word] += 1 }
-    list
+    remove_num.scan(/\w+/) { |word| list[word] += 1 }
+    list.sort_by(&:last).reverse
   end
-  
+
   def amount_of_words 
     reader.scan(/\w+/)
   end
